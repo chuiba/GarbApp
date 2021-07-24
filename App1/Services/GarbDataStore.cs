b0d39ab5ff3bb4f1c04f232b2b666d23edf848a5
chuiba <chuibachuibachuiba@163.com>
2021-07-24 23:32:44 +0800

    添加项目文件。

---
+++ App1/Services/GarbDataStore.cs
@@ -0,0 +1,36 @@
+﻿using App1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1.Services
+{
+    class GarbDataStore : IDataStore<Item>
+    {
+        readonly List<Item> items;
+
+        public GarbDataStore()
+        {
+            // 获取所有的装扮信息
+
+            items = new List<Item>()
+            {
+                new Item { Id = "1", Text = "呜米", Description="12345" },
+                new Item { Id = "1", Text = "星座系列：狮子座", Description="54321" },
+            };
+        }
+
+
+        public async Task<Item> GetItemAsync(string id)
+        {
+            return await Task.FromResult(items.FirstOrDefault(s => s.Id == id));
+        }
+
+        public async Task<IEnumerable<Item>> GetItemsAsync(bool forceRefresh = false)
+        {
+            return await Task.FromResult(items);
+        }
+    }
+}
---
+++ App1/ViewModels/AboutViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace App1.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = "主页";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://www.bilibili.com/h5/mall/home?navhide=1"));
+            OpenAuthorCommand = new Command(async () => await Browser.OpenAsync("https://space.bilibili.com/4338056/dynamic"));
+        }
+
+        public ICommand OpenWebCommand { get; }
+        public ICommand OpenAuthorCommand { get; }
+    }
+}
