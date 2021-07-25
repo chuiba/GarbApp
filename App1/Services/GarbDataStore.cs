0286d24a523799f3b2261ae2f27b98216e25eed3
chuiba <chuibachuibachuiba@163.com>
2021-07-25 16:46:27 +0800

    feat: 图标和名称修改

--- App1/Services/GarbDataStore.cs
+++ App1/Services/GarbDataStore.cs
@@ -1,9 +1,15 @@
 ﻿using App1.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace App1.Services
 {
@@ -11,17 +17,46 @@ namespace App1.Services
     {
         readonly List<Item> items;
 
+        private void updateGarbInfo()
+        {
+            items.Clear();
+
+            WebClient client = new WebClient();
+            Stream stream = client.OpenRead("https://www.bilibili.com/h5/mall/home?navhide=1");
+            StreamReader ss = new StreamReader(stream);
+
+            string html = ss.ReadToEnd();
+            // Console.WriteLine(html);
+
+            string pattern = "{\"item_id\":[^0].*?sale_surplus\":\\d+}";
+            foreach (Match match in Regex.Matches(html, pattern))
+            {
+                JObject jo = (JObject)JsonConvert.DeserializeObject(match.Value);
+                int surplus = int.Parse(jo["sale_surplus"].ToString());
+                int quantity = int.Parse(jo["properties"]["sale_quantity"].ToString());
+                // 编号 = 总量 - 库存; 另外需要注意星座等没有库存限制的 
+                int number = quantity - surplus;
+
+                if (surplus <= 0)
+                    continue;
+
+                items.Add(new Item
+                {
+                    Id = jo["item_id"].ToString(),
+                    Text = jo["name"].ToString(),
+                    Description = number.ToString()
+                });
+            }
+        }
+
+
         public GarbDataStore()
         {
             // 获取所有的装扮信息
+            items = new List<Item>();
 
-            items = new List<Item>()
-            {
-                new Item { Id = "1", Text = "呜米", Description="12345" },
-                new Item { Id = "1", Text = "星座系列：狮子座", Description="54321" },
-            };
+            updateGarbInfo();
         }
-
 
         public async Task<Item> GetItemAsync(string id)
         {
@@ -30,6 +65,7 @@ namespace App1.Services
 
         public async Task<IEnumerable<Item>> GetItemsAsync(bool forceRefresh = false)
         {
+            updateGarbInfo();
             return await Task.FromResult(items);
         }
     }
